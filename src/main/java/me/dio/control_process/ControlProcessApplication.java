34088deeec2ee64c538b5266b9b6e7e80f596cf4
Rafael Moreira <rafaeltimoreirass@gmail.com>
2025-04-05 03:04:21 -0300

    Initial commit

---
+++ src/main/java/me/dio/control_process/ControlProcessApplication.java
@@ -0,0 +1,13 @@
+package me.dio.control_process;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlProcessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlProcessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/dio/control_process/ControlProcessApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.control_process;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlProcessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
