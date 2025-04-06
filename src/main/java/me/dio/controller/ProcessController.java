d2f53bf8f9faf1e7626d10abf15ee2ade02e2938
Rafael Moreira <rafaeltimoreirass@gmail.com>
2025-04-06 17:13:53 -0300

    Deploy v.1 na plataforma Railway

--- src/main/java/me/dio/controller/ProcessController.java
+++ src/main/java/me/dio/controller/ProcessController.java
@@ -10,7 +10,7 @@ import java.net.URI;
 import java.util.List;
 
 @RestController
-@RequestMapping("/processes")
+@RequestMapping("process-data")
 public class ProcessController {
 
   private final ProcessService processService;
@@ -23,6 +23,16 @@ public class ProcessController {
   public ResponseEntity<Process> findById(@PathVariable Long id) {
     var process = processService.findById(id);
     return ResponseEntity.ok(process);
+  }
+
+  @GetMapping("/status/{status}")
+  public ResponseEntity<List<Process>> findByStatus(@PathVariable String status) {
+    return ResponseEntity.ok(processService.findByStatus(status));
+  }
+
+  @GetMapping("/responsavel/{responsavel}")
+  public ResponseEntity<List<Process>> findByResponsavel(@PathVariable String responsavel) {
+    return ResponseEntity.ok(processService.findByResponsavel(responsavel));
   }
 
   @PostMapping
