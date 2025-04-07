6c6e1ebdbf9e693a03d867f216f0e1d2c69f2e00
Rafael Moreira <rafaeltimoreirass@gmail.com>
2025-04-07 13:50:33 -0300

    Deploy v.12 na plataforma Railway

--- src/main/java/me/dio/controller/ProcessController.java
+++ src/main/java/me/dio/controller/ProcessController.java
@@ -1,5 +1,7 @@
 package me.dio.controller;
 
+import jakarta.validation.Valid;
+import me.dio.controller.dto.ProcessDTO;
 import me.dio.domain.model.Process;
 import me.dio.service.ProcessService;
 import org.springframework.http.ResponseEntity;
@@ -35,13 +37,35 @@ public class ProcessController {
     return ResponseEntity.ok(processService.findByResponsavel(responsavel));
   }
 
+//  @PostMapping
+//  public ResponseEntity<Process> create(@RequestBody Process processToCreate) {
+//    var createdProcess = processService.create(processToCreate);
+//    URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+//            .path("/{id}")
+//            .buildAndExpand(createdProcess.getId())
+//            .toUri();
+//    return ResponseEntity.created(location).body(createdProcess);
+//  }
   @PostMapping
-  public ResponseEntity<Process> create(@RequestBody Process processToCreate) {
+  public ResponseEntity<Process> create(@RequestBody @Valid ProcessDTO dto) {
+    Process processToCreate = new Process();
+
+    processToCreate.setNumeroProcesso(dto.getNumeroProcesso());
+    processToCreate.setTitulo(dto.getTitulo());
+    processToCreate.setGerencia(dto.getGerencia());
+    processToCreate.setStatus(dto.getStatus());
+    processToCreate.setPrioridade(dto.getPrioridade());
+    processToCreate.setDataCriacao(dto.getDataCriacao());
+    processToCreate.setDataVencimento(dto.getDataVencimento());
+    processToCreate.setResponsavel(dto.getResponsavel());
+    processToCreate.setDescricao(dto.getDescricao());
+
     var createdProcess = processService.create(processToCreate);
     URI location = ServletUriComponentsBuilder.fromCurrentRequest()
             .path("/{id}")
             .buildAndExpand(createdProcess.getId())
             .toUri();
+
     return ResponseEntity.created(location).body(createdProcess);
   }
 }
--- src/main/java/me/dio/controller/exception/GlobalExceptionHandler.java
+++ src/main/java/me/dio/controller/exception/GlobalExceptionHandler.java
@@ -4,6 +4,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -28,5 +30,14 @@ public class GlobalExceptionHandler {
     var message = "Unexpected server error, see the logs.";
     logger.error(message, unexpectedException);
     return new ResponseEntity<>(message, HttpStatus.INTERNAL_SERVER_ERROR);
+  }
+
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ResponseEntity<String> handleValidationException(MethodArgumentNotValidException ex) {
+    StringBuilder errors = new StringBuilder("Erro(s) de validação: ");
+    for (FieldError error : ex.getBindingResult().getFieldErrors()) {
+      errors.append(String.format("[%s: %s] ", error.getField(), error.getDefaultMessage()));
+    }
+    return new ResponseEntity<>(errors.toString(), HttpStatus.BAD_REQUEST);
   }
 }
