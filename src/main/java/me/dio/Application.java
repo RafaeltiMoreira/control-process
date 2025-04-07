180fec3d3380818d8321121298d8d3ac6bad0e17
Rafael Moreira <rafaeltimoreirass@gmail.com>
2025-04-06 23:43:55 -0300

    Deploy v.5 na plataforma Railway

--- src/main/java/me/dio/Application.java
+++ src/main/java/me/dio/Application.java
@@ -7,10 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @OpenAPIDefinition(servers = { @Server(url = "/", description = "Default Server URL")})
 @SpringBootApplication
-public class ProcessApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProcessApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
